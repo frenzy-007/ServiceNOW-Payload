525b9d67809ef30873e1bb6e7a405bb9fb523a3c
Menghour <menghourpech007@gmail.com>
2025-11-11 12:51:18 +0100

    some message describing what you changed

---
+++ api/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API working on Vercel!");
+});
+
+app.get("/about", (req, res) => {
+  res.json({
+    "category-actions": {
+      name: "Navigate To URL",
+      type: "navigateTo",
+      value: "Error! Hyperlink reference not valid.",
+      foreground: true,
+      authentication: true,
+      destructive: false,
+    },
+    dummyData: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ".repeat(1000),
+      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ".repeat(
+        1000
+      ),
+      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip. ".repeat(
+        1000
+      ),
+      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat. ".repeat(
+        1000
+      ),
+      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt. ".repeat(
+        1000
+      ),
+    ],
+  });
+});
+
+module.exports = app;
